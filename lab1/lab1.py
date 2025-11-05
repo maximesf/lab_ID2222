7011eb4e7c7e6963c6e206640bf04c978ffc3ba4
maximesf <maxime.sf14@gmail.com>
2025-11-05 07:29:36 +0100

    minHashing and compareSigntures class

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -16,7 +16,7 @@ class Shingling:
             reader = csv.reader(f)
             row = next(reader)
             oneShingle = list(row)
-            print(oneShingle)
+            #print(oneShingle)
         text = oneShingle[0]
         kShingle = []
         for i in range(len(text)):
@@ -41,7 +41,7 @@ class Shingling:
     def uniqueHashShingling(self, csvFile: str) -> list[int]:
         hashShingle = []
         kShingle = self.kShingling(csvFile)
-        print(kShingle)
+        #print(kShingle)
         kShingle = np.unique(kShingle)
         for word in kShingle:
             res = h.md5(word.encode())
@@ -51,7 +51,7 @@ class Shingling:
     
 class CompareSets:
     def __init__(self) -> None:
-        print('Done')
+        print('Compare Sets class')
         
     def getJaccardSim(self,set1,set2) -> float:
         inter = 0
@@ -61,20 +61,66 @@ class CompareSets:
                 if(hash1==hash2):
                     inter += 1
         union = len(np.unique(set1 + set2))
-
-        print(inter/union)
         return inter/union
+    
+class MinHashing:
+    def __init__(self) -> None:
+        print('MinHashing class')
+        
+    def buildSignature(self,set1,set2,nbPermut) -> float:
+        
+        union = np.unique(set1 + set2)
+        mask1 = np.isin(union, set1)
+        mask2 = np.isin(union, set2)
+        
+        signatureMatrix = np.empty((0, 2), int)
+        for _ in range(nbPermut):
+            permutedIndexes = np.random.permutation(len(union))
+            lineSignatureMatrix = np.zeros((1,2))
+            for i in range(len(permutedIndexes)):
+                if(mask1[permutedIndexes[i]]):
+                    lineSignatureMatrix[0][0]=i
+                    break
+            for j in range(len(permutedIndexes)):
+                if(mask2[permutedIndexes[j]]):
+                    lineSignatureMatrix[0][1]=j
+                    break
+            signatureMatrix = np.vstack((signatureMatrix, lineSignatureMatrix))
 
+        return signatureMatrix
+
+class CompareSignatures:
+    def __init__(self) -> None:
+        print('Compare Signatures class')
+        
+    def computeEstimateSimilarity(self,signature) -> float:
+        
+        signature = signature.T
+
+        y = signature[0]-signature[1]
+        y = np.where(y==0,1,0)
+        
+        return np.sum(y)/len(y)
+    
+estimator = CompareSignatures()
+miniHasher = MinHashing()
 comparator = CompareSets()
 
 fiveShingler = Shingling(5)
 #kShingle = fiveShingler.kShingling('test.csv')
+hashList = fiveShingler.uniqueHashShingling('test.csv')
+#print(f"len = {len(hashList)}")
 hashShingle = fiveShingler.uniqueHashShingling('test.csv')
-email2Hash = fiveShingler.uniqueHashShingling('test.csv')
+test3 = fiveShingler.uniqueHashShingling('test2.csv')
 #print(kShingle)
 #print(hashShingle)
 
-similarity = comparator.getJaccardSim(hashShingle,email2Hash)
+similarity = comparator.getJaccardSim(hashShingle,test3)
+signature = miniHasher.buildSignature(hashShingle,test3,100000)
+estimate = estimator.computeEstimateSimilarity(signature)
+#print(signature)
+print(similarity)
+print(estimate)
 """
 word = 'test'
 word2 = 'test'
