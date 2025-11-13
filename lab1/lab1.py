59f9ecd2c75b66449d0515a017820b07c759a779
maximesf <maxime.sf14@gmail.com>
2025-11-13 09:55:18 +0100

    some plotting

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -1,6 +1,9 @@
 import numpy as np
 import csv
 import hashlib as h
+import time
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 class Shingling:
     k: int
@@ -14,16 +17,21 @@ class Shingling:
         self.word = word
         self.removeCaracter = '()+=-_!,;.:/?"'
     
-    def kShingling(self, csvFile: str) -> list[str]:
-        oneShingle = []
-        with open(csvFile, newline='') as f:
-            reader = csv.reader(f)
-            row = next(reader)
-            oneShingle = list(row)
-            #print(oneShingle)
-        text = oneShingle[0]
-        kShingle = []
+    def kShingling(self, file: str, csvFile : bool = False ) -> list[str]:
+        if(csvFile):
+            oneShingle = []
+            with open(file, newline='') as f:
+                reader = csv.reader(f)
+                row = next(reader)
+                oneShingle = list(row)
+                #print(oneShingle)
+            text = oneShingle[0]
+        else:
+            with open(file, "r", encoding="utf-8") as file:
+                text = file.read()
+                text = text.replace("\n"," ")
 
+        kShingle = []
         text = text.lower()
         
         for i in range(len(self.removeCaracter)):
@@ -32,7 +40,7 @@ class Shingling:
         if(self.word):
             wordList = text.split(" ")
             for i in range(len(wordList)):
-                if(i+self.k>len(wordList)):
+                if(i+self.k+1>=len(wordList)):
                     self.shingle = kShingle
                     #print(kShingle)
                     return kShingle
@@ -43,7 +51,7 @@ class Shingling:
                 kShingle.append(key)
         else:
             for i in range(len(text)):
-                if(i+self.k>len(text)):
+                if(i+self.k+1>=len(text)):
                     self.shingle = kShingle
                     #print(kShingle)
                     return kShingle
@@ -162,17 +170,13 @@ estimator = CompareSignatures()
 miniHasher = MinHashing()
 comparator = CompareSets()
 
-fiveShingler = Shingling(3,True)
-#kShingle = fiveShingler.kShingling('test.csv')
-#hashList = fiveShingler.uniqueHashShingling('test.csv')
-#print(f"len = {len(hashList)}")
-hashShingle = fiveShingler.uniqueHashShingling('test.csv')
-test3 = fiveShingler.uniqueHashShingling('test2.csv')
-#print(kShingle)
-#print(hashShingle)
+fiveShingler = Shingling(10,False)
+shingled = fiveShingler.kShingling('1.txt',False)
+hashShingle = fiveShingler.uniqueHashShingling('1.txt')
+test3 = fiveShingler.uniqueHashShingling('2.txt')
 
 similarity = comparator.getJaccardSim(hashShingle,test3)
-signature = miniHasher.buildSignature(hashShingle,test3,10000)
+signature = miniHasher.buildSignature(hashShingle,test3,100)
 estimate = estimator.computeEstimateSimilarity(signature)
 #print(f"longueur de la signature {len(signature)}")
 retour = LSHasher.lookForCandidates(signature)
@@ -181,15 +185,118 @@ print(f"r = {LSHasher.r}, retour de LSH {retour}")
 #print(signature)
 print(similarity)
 print(estimate)
-"""
-word = 'test'
-word2 = 'test'
-hash = h.md5(word.encode())
-hash2 = h.md5(word2.encode())
-print(hash.hexdigest())
-print(hash2.hexdigest())
+
+"""docs = ['00'+str(i)+'.txt' for i in range(1,10)]
+print(docs)
+comparedHash = fiveShingler.uniqueHashShingling('data/politics/010.txt')
+
+e = time.time()
+for doc in docs:
+    hash = fiveShingler.uniqueHashShingling('data/politics/'+doc)
+    signature = miniHasher.buildSignature(comparedHash,hash,100)
+    estimate = estimator.computeEstimateSimilarity(signature)
+    if(estimate>=0.2):
+        print(doc)
+print((time.time()-e)*1000)"""
 
 
-if(hash.hexdigest() == hash2.hexdigest()):
-    print('le meme hash')
+"""permuts = [10**i for i in range(1,6)]
+permuts = [10, 100, 1000, 10000, 40000, 70000, 100000]
+docs = ['10.txt','200.txt','1000.txt','5000.txt']
+numberWords = [10,200,1000,5000]
+fiveShingler = Shingling(10,False)
+for i in range(1,10,3):
+    computeEstimate=[]
+    for doc in docs:
+        shingler = Shingling(i,True)
+        e = time.time()
+        comparedHash = shingler.uniqueHashShingling('data/lenght/'+doc)
+        computeEstimate.append((time.time()-e)*1000)
+    plt.plot(numberWords,computeEstimate,label=f"k = {i}")
+
+plt.xlabel("Number of word")
+plt.legend()
+plt.ylabel("time in ms")
+plt.show()"""
+
 """
+docs = ['10.txt','200.txt','1000.txt','2500.txt','5000.txt']
+numberWords = [10,200,1000,2500,5000]
+fiveShingler = Shingling(10,False)
+
+computeEstimate=[]
+for doc in docs:
+    e = time.time()
+
+    for i in range(10):
+        comparedHash = fiveShingler.uniqueHashShingling('data/lenght/'+doc)
+        sim = comparator.getJaccardSim(hashShingle,comparedHash)
+        # signature = miniHasher.buildSignature(hashShingle,comparedHash,100)
+        # estimate = estimator.computeEstimateSimilarity(signature)
+
+    computeEstimate.append((time.time()-e)*100)
+print(computeEstimate)
+plt.scatter(numberWords,computeEstimate)
+
+plt.xlabel("Number of words")
+plt.legend()
+plt.ylabel("time in ms")
+plt.show()
+"""
+
+permuts = [10, 100, 1000, 10000, 40000, 70000, 100000,150000,300000]
+computeEstimate=[]
+estimates = []
+
+for i in permuts:
+    e = time.time()
+    signature = miniHasher.buildSignature(hashShingle,test3,i)
+    estimate = estimator.computeEstimateSimilarity(signature)
+    computeEstimate.append((time.time()-e)*1000)
+    estimates.append(estimate)
+
+jac = comparator.getJaccardSim(hashShingle,test3)
+errorVector = np.abs(np.array(estimates)-jac)/jac*100
+# fig = plt.figure()
+# ax = plt.gca()
+# ax.scatter(permuts ,compute , c='blue', alpha=0.05, edgecolors='none')
+# ax.set_yscale('log')
+# ax.set_xscale('log')
+plt.scatter(permuts,computeEstimate,colorizer='blue')
+plt.xlabel("number of permutation")
+plt.ylabel("time in ms")
+plt.show()
+
+plt.scatter(permuts,errorVector,colorizer='blue')
+plt.xlabel("number of permutation")
+plt.ylabel("Relative error")
+plt.show()
+
+"""
+computeEstimate=[]
+computeLSH=[]
+docs = ['10.txt','200.txt','1000.txt']
+for doci in docs:
+    e = time.time()
+    comparedHash = fiveShingler.uniqueHashShingling('data/lenght/'+doci)
+    temp = []
+    tempLSH = []
+    for docj in docs:
+        hash = fiveShingler.uniqueHashShingling('data/lenght/'+docj)
+        signature = miniHasher.buildSignature(comparedHash,hash,100000)
+        
+        estimate = estimator.computeEstimateSimilarity(signature)
+        temp.append((time.time()-e)*1000)
+        e = time.time()
+        candidates = LSHasher.lookForCandidates(signature)
+        tempLSH.append((time.time()-e)*1000)
+    computeEstimate.append(temp)
+    computeLSH.append(tempLSH)
+
+sns.heatmap(computeLSH, annot=True, xticklabels=docs, yticklabels=docs, cmap="viridis")
+plt.title("Computation time LSH")
+plt.show()
+
+sns.heatmap(computeEstimate, annot=True, xticklabels=docs, yticklabels=docs, cmap="viridis")
+plt.title("Computation time LSH")
+plt.show()"""
